61bd2d57e80bbd45b4427d451aa527812b1857a9
sonthth1903012 <sonthth1903012@fpt.edu.vn>
2020-05-02 23:56:01 +0700

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,20 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        DB::table('users')->insert([
-//            'email' => 'admin@company.com',
-//            'name' => 'admin',
-//            'email_verified_at' => '2020-1-1 23:59:59',
-//            'password' => Hash::make('12345678'),
-//            'role' => '2',
-//            'phone'=> '0123456789',
-//            'address' => "Ha Noi"
-//        ]);
+        DB::table('users')->insert([
+            'email' => 'admin@company.com',
+            'name' => 'admin',
+            'email_verified_at' => '2020-1-1 23:59:59',
+            'password' => Hash::make('12345678'),
+            'role' => '2',
+            'phone'=> '0123456789',
+            'address' => "Ha Noi"
+        ]);
 
-        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
-        $this->call(CommentSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(PostSeeder::class);
+        $this->call(CommentSeeder::class);
+
 
     }
 }
